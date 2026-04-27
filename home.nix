55cb0f3ce78d6e9f43870f9c8ed4693323597704
Allain <allain@users.noreply.github.com>
2026-04-27 10:45:59 -0400

    Install Codex via user npm prefix

--- home.nix
+++ home.nix
@@ -1,7 +1,28 @@
 { nvchad-starter }:
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 {
   home.stateVersion = "25.11";
+
+  home.packages = with pkgs; [
+    nodejs_22
+  ];
+
+  home.sessionPath = [
+    "$HOME/.npm-global/bin"
+  ];
+
+  home.file.".npmrc".text = "prefix=${config.home.homeDirectory}/.npm-global\n";
+
+  home.activation.installOpenAICodex = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    export NPM_CONFIG_PREFIX="${config.home.homeDirectory}/.npm-global"
+    export PATH="${pkgs.nodejs_22}/bin:$NPM_CONFIG_PREFIX/bin:$PATH"
+
+    $DRY_RUN_CMD mkdir -p "$NPM_CONFIG_PREFIX"
+
+    if [ ! -x "$NPM_CONFIG_PREFIX/bin/codex" ]; then
+      $DRY_RUN_CMD ${pkgs.nodejs_22}/bin/npm install --global @openai/codex
+    fi
+  '';
 
   programs.git = {
     enable = true;
